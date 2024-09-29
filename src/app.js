11c005db50a59b85d1cc5368653872b4c53bc07c
WistonTravieso <wistontravieso@gmail.com>
2024-09-29 20:05:47 +0000

    [Cartas Aleatorias]

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function paloRandom() {
+    let palo = ["♦", "♥", "♠", "♣"];
+    let indexRandom = Math.floor(Math.random() * 4);
+
+    let paloTop = document.querySelector("#palotop");
+    paloTop.textContent = palo[indexRandom];
+    console.log(paloTop);
+
+    if (indexRandom == 0) {
+      paloTop.style.color = "red";
+    }
+
+    let paloEnd = document.querySelector("#paloend");
+    paloEnd.textContent = palo[indexRandom];
+  }
+  paloRandom();
+
+  function numberRandom() {
+    let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+    let indexRandom = Math.floor(Math.random() * 12);
+    return number[indexRandom];
+  }
+
+  let valueCard = document.querySelector("#valueCard");
+  valueCard.textContent = numberRandom();
+
+  console.log(numberRandom() + paloRandom());
 };
